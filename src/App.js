bbc89254d62dad8515943db7a04d4dd0587d5633
amdouni-chouaib <chouaibamdouni@gmail.com>
2021-08-13 23:30:16 +0100

    react props

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ProfileComponent from './profile/ProfileComponent';
+
+
+
+function App(){
+  return(
+    <div>
+  <ProfileComponent name={"Amdouni Chouaib"} bio={"web developer and engineering DevOps"} profession={"3 years of freelance "}/>
+    </div>
+  );
+}
+export default App;
